c8af6b2939b33b93921bebeae147575fcba148f0
delremm <g1m5yv@gmail.com>
2014-01-19 14:01:49 +0600

    ss

--- hd_app/views.py
+++ hd_app/views.py
@@ -37,7 +37,8 @@ class IndexView(TemplateView):
                     csv_writer = csv.writer(csv_file, delimiter=',')
                     for su in startups:
                         try:
-                            csv_writer.writerow([su.name, str(su.created_at)[:10], su.follower_count])
+                            if su.name and su.created_at and su.follower_count
+                                csv_writer.writerow([su.name, str(su.created_at)[:10], su.follower_count])
                         except:
                             pass
                     context['csv_file'] = '%s%s'  % (settings.MEDIA_URL, csv_file_name)
